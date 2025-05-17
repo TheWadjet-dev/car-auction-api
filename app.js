29ad9c1f1fbfe2aad1c0e91343a92fe9d6e15c5b
SrNexar <69290901+TheWadjet-dev@users.noreply.github.com>
2025-05-17 18:52:57 -0500

    changes

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ import pagoRoutes from './routes/pago.routes.js';
 const app = express();
 app.use(express.json());
 
-app.use('/api/clientes', clienteRoutes);
 app.use('/api/carros', carroRoutes);
+app.use('/api/clientes', clienteRoutes);
 app.use('/api/pujas', pujaRoutes);
 app.use('/api/pago', pagoRoutes);
+
+app.get('/', (req, res) => res.send('API de Subastas de Carros'));
 
 export default app;
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
