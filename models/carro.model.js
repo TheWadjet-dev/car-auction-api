7526b6d8b33985dd18a8c67ae76e4aa1ba2c9bcf
Stiwar Saltos <69290901+TheWadjet-dev@users.noreply.github.com>
2025-05-21 10:03:21 -0500

    fix car model

--- models/carro.model.js
+++ models/carro.model.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
+  marca: String,
   modelo: String,
+  año: Number,
   precioInicial: Number,
   ownerWallet: String,
   estado: { type: String, default: 'activo' },
